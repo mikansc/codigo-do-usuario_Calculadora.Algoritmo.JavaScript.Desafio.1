98cf89aa5cecc243ef8ce98071a64ef261757cc7
Michael Nascimento <michael.nsc@outlook.com>
2024-08-13 19:23:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Calculadora Simples em JavaScript
+
+## Pré requisitos
+
+- Navegador web (Chrome, Firefox, etc)
+  OU
+- NodeJS instalado
+
+## Como rodar
+
+Para rodar o código, você pode copiar e colar a implementação no Console do navegador ou, caso tenha o Node instalado, executar o comando `node calculadora.js` no terminal na pasta raiz do projeto.
---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function soma(a, b) {
+  // Implemente a adição
+}
+
+function subtracao(a, b) {
+  // Implemente a subtração
+}
+
+function divisao(a, b) {
+  // Implemente a divisão
+  // Lembre-se de retornar null se b for igual a 0
+}
+
+function multiplicacao(a, b) {
+  // Implemente a multiplicação
+}
+
+console.log("Adição: " + soma(10, 5)); // Saída esperada: 15
+console.log("Subtração: " + subtracao(10, 5)); // Saída esperada: 5
+console.log("Divisão: " + divisao(10, 5)); // Saída esperada: 2
+console.log("Divisão por zero: " + divisao(10, 0)); // Saída esperada: null
+console.log("Multiplicação: " + multiplicacao(10, 5)); // Saída esperada: 50
